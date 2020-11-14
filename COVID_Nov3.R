85667a6f9e1c90aeb7057ab04a0e84ffd7f15107
hybras <24651269+hybras@users.noreply.github.com>
2020-11-14 16:41:19 -0500

    Change install to library for 'maps'

--- COVID_Nov3.R
+++ COVID_Nov3.R
@@ -1,6 +1,4 @@
-# install.packages("maps")
 # RUN THIS BEFORE RUNNING THE CODE BELOW
-
 library(dplyr)
 library(ggplot2)
 library(tidyverse)
@@ -10,6 +8,7 @@ library(gifski)
 library(png)
 library(ggrepel)
 library(scales)
+library(maps)
 #change the path below to the address to the file on your computer.
 
 path <- "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
