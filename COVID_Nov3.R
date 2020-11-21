972f7dda5c149bdd471de818e74cf30fdd8eca86
ehuegler <43831135+ehuegler@users.noreply.github.com>
2020-11-21 11:47:06 -0500

    Normalize case data to 100,000 people. With graphs (#2)
    * Update COVID_Nov3.R
    
    Added state population info and a new graph which adjusts each states case count to cases per 100,000 people to allow better comparison between different sized states.
    
    * Update COVID_Nov3.R
    
    added animation too
    
    * Fixed the dataset link

--- COVID_Nov3.R
+++ COVID_Nov3.R
@@ -46,15 +46,37 @@ covid_cont_state.long_1 <- pivot_longer(
   values_to = "cases"
 )
 
-
 #Choose states to plot
-plot_states <- c("new york", "florida", "texas", "california")
+plot_states <- c("new york", "florida", "texas", "california", "maryland")
 
 #data for plotting specific states
 covid_plot_data_1 <-
   covid_cont_state.long_1 %>% filter(region == plot_states)
 
+# adjust data based on state population
+
+# gets state populations
+path <- "https://www2.census.gov/programs-surveys/popest/datasets/2010-2019/state/detail/SCPRC-EST2019-18+POP-RES.csv"
+state_data <- read.csv(path)
+state_data$NAME <- tolower(state_data$NAME)
+
+region <- vector()
+date <- vector()
+cases <- vector()
+covid_plot_data_adjusted <- data.frame(region, date, cases)
+
+# for each selected state it divides the cases by that states
+# population and multiplies by 100,000
+for (state in plot_states) {
+  covid_plot_data_adjusted <- merge(covid_plot_data_adjusted,
+                                    mutate(filter(covid_plot_data_1, region == state),
+                                           adjusted = (cases / filter(state_data, NAME == state)$POPESTIMATE2019) * 100000),
+                                    all = TRUE)
+}
+
 #Plot the data
+
+#standard
 q_1 <- covid_plot_data_1 %>%
   ggplot(aes(
     x = as.Date(date),
@@ -69,13 +91,46 @@ q_1 <- covid_plot_data_1 %>%
     y = "Number of Cases"
   ) +
   geom_line() +
-  geom_point() +
-  geom_text(
-    aes(label = factor(region)),
-    hjust = 0,
-    position = position_dodge(width = 0.9),
-    size = 4
+  # geom_point() +
+  # geom_text(
+  #   aes(label = factor(region)),
+  #   hjust = 0,
+  #   position = position_dodge(width = 0.9),
+  #   size = 4
+  # ) +
+  scale_x_date(limits = as.Date(c("2020-1-22", "2020-11- 02")),
+               date_breaks = "1 month",
+               date_labels = "%B") +
+  scale_y_continuous(labels = scales::comma) +
+  theme(
+    plot.title = element_text(color = "black", size = 14, face = "bold.italic"),
+    axis.title.x = element_text(color = "black", size = 10),
+    axis.title.y = element_text(color = "black", size = 10),
+    legend.position = "none"
+  )
+
+# with cases adjusted per 100,000 people
+q_2 <- covid_plot_data_adjusted %>%
+  ggplot(aes(
+    x = as.Date(date),
+    y = adjusted,
+    group = region,
+    color = region
+  )) +
+  labs(
+    title = "Time Series Plot of Confirmed Cases of COVID per 100,000 People",
+    subtitle = "Jonathan Fernandes PhD, University of Maryland, College Park.",
+    x = "Date",
+    y = "Number of Cases per 100,000 People"
   ) +
+  geom_line() +
+  # geom_point() +
+  # geom_text(
+  #   aes(label = factor(region)),
+  #   hjust = 0,
+  #   position = position_dodge(width = 0.9),
+  #   size = 4
+  # ) +
   scale_x_date(limits = as.Date(c("2020-1-22", "2020-11- 02")),
                date_breaks = "1 month",
                date_labels = "%B") +
@@ -112,7 +167,41 @@ p_1 <- covid_plot_data_1 %>%
     axis.title.y = element_text(color = "black", size = 10)
   )
 
+p_2 <- covid_plot_data_adjusted %>%
+  ggplot(aes(
+    x = as.Date(date),
+    y = adjusted,
+    group = region,
+    color = region
+  )) +
+  labs(
+    title = "Time Series Plot of Confirmed Cases of COVID",
+    subtitle = "Jonathan Fernandes PhD, University of Maryland, College Park.",
+    x = "Date",
+    y = "Number of Cases per 100,000 People"
+  ) +
+  geom_line() +
+  scale_x_date(limits = as.Date(c("2020-1-22", "2020-11- 02")),
+               date_breaks = "1 month",
+               date_labels = "%B") +
+  scale_y_continuous(labels = scales::comma) +
+  theme(
+    plot.title = element_text(color = "black", size = 14, face = "bold.italic"),
+    axis.title.x = element_text(color = "black", size = 10),
+    axis.title.y = element_text(color = "black", size = 10)
+  )
+
 q_1 <- q_1 +  theme(
+  #panel.background = element_rect(fill = "white"),
+  plot.margin = margin(5, 10 , 5, 1, "mm"),
+  plot.background = element_rect(
+    fill = "white",
+    colour = "black",
+    size = 1
+  )
+)
+
+q_2 <- q_2 +  theme(
   #panel.background = element_rect(fill = "white"),
   plot.margin = margin(5, 10 , 5, 1, "mm"),
   plot.background = element_rect(
@@ -126,6 +215,16 @@ q_1 <- q_1 +  theme(
 anim_1 <- q_1 + transition_reveal(as.Date(date))
 animation_1 <- animate(
   anim_1,
+  width = 750,
+  height = 650,
+  duration = 20,
+  end_pause = 20,
+  renderer = gifski_renderer()
+)
+
+anim_2 <- q_2 + transition_reveal(as.Date(date))
+animation_2 <- animate(
+  anim_2,
   width = 750,
   height = 650,
   duration = 20,
